c3c4318a914b3707660345c38b89a222c0cc40d6
David Guenin <guenin.david@gmail.com>
2020-03-27 23:54:22 +0100

    Continue first draft

--- src/commands/yoworld.ts
+++ src/commands/yoworld.ts
@@ -6,19 +6,18 @@ export class MyCommand extends Command {
 
   async run() {
 
-
+    //INIT AIRTABLE
     var Airtable = require('airtable');
     var base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base('appcKg4FJ08P9Ygxa');
 
-    //PAGINATION
-    //https://stackoverflow.com/questions/42761068/paginate-javascript-array
+
+    //PAGINATION FUNCTION
     function paginate(array, page_size, page_number) {
 
       var sliceIt = array.slice((page_number - 1) * page_size, page_number * page_size);
 
       return sliceIt;
     }
-
 
     //BLESSED LAYOUT
     // Create a screen object.
@@ -28,100 +27,71 @@ export class MyCommand extends Command {
 
     screen.title = 'Nectune homepage';
 
-    // Create a box perfectly centered horizontally and vertically.
     var mainHome = blessed.box({
       top: '0%',
       left: 'center',
-      width: '50%',
+      width: '60%',
       height: '100%',
-      text: 'Hello {bold}world{/bold}!',
-      tags: true,
       scrollable: true,
-      border: {
-        type: 'line'
+      keys: true,
+      alwaysScroll: true,
+      scrollbar: {
+        bg: 'blue'
       },
-      style: {
-        fg: 'white',
-        bg: 'magenta',
-        border: {
-          fg: '#f0f0f0'
-        }
-      }
     });
 
-    // Quit on Escape, q, or Control-C.
     screen.key(['escape', 'q', 'C-c'], function(ch, key) {
       return process.exit(0);
     });
-
-
-    // Append our box to the screen.
-    screen.append(mainHome );
     
 
+
+    screen.append(mainHome);
+    
+
+    //START AIRTABLE LOOP
     base('Yoworld').select({
-        // Selecting the first 3 records in Grid view:
         view: "Grid view"
     }).eachPage(function page(records, fetchNextPage) {
 
-      // This function (`page`) will get called for each page of records.
-
-      // To fetch the next page of records, call `fetchNextPage`.
-      // If there are more records, `page` will get called again.
-      // If there are no more records, `done` will get called.
       fetchNextPage();
 
         //MAP RECORD
-        //https://stackoverflow.com/questions/23036023/javascript-array-of-objects-get-single-value
         var ids = records.map(function(i) {
           return i.get('Name - Lastname');
         });
 
-        var tt = paginate(ids, 5, 1)
+        //PAGINATE SORT RECORD
+        var listCards = paginate(ids, 5, 1)
 
-
-        var number = 0;
+        //LOOP RECORD
         var i = 0;
-        while (i < tt.length) {
+        while (i < listCards.length) {
           
+          //CARD
           var boxTwo = blessed.box({
             parent: mainHome,
-            top: (i * 10) + '%',
+            top: (i * 35) + '%',
             left: 'center',
-            width: '100%',
-            height: '10%',
-            content: 'Hello' + i,
-            tags: true,
-            border: {
-              type: 'line'
-            },
+            width: '95%',
+            height: '33%',
+            content: listCards[i],
             style: {
               fg: '#000',
               bg: '#efefef',
-              border: {
-                fg: '#000'
-              },
             }
-        });
+          });
 
-          i++;
 
-      
+        //LOOP RECORD END
+        i++;
         }
-
-
-
-
 
       // Render the screen.
       screen.render();
               
-      //console.log(paginate(ids, 1, 1).join("\n"));
-
-      console.log(tt)
-        
-
-
+      
+    //END AIRTABLE LOOP
     }, function done(err) {
         if (err) { console.error(err); return; }
     });
