fa050521f7056f04031acc5811f4fdca85c699ea
David Guenin <guenin.david@gmail.com>
2020-04-12 15:34:34 +0200

    first api draft

--- src/commands/yoworld.ts
+++ src/commands/yoworld.ts
@@ -1,132 +1,136 @@
 import Command from '@oclif/command'
 require('dotenv').config()
+const fetch = require('node-fetch');
 var blessed = require('blessed');
 
 export class MyCommand extends Command {
 
   async run() {
 
-    //INIT AIRTABLE
-    var Airtable = require('airtable');
-    var base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base('appcKg4FJ08P9Ygxa');
-
-
-    //PAGINATION FUNCTION
-    function paginate(array, page_size, page_number) {
-
-      var sliceIt = array.slice((page_number - 1) * page_size, page_number * page_size);
-
-      return sliceIt;
+//     let test;
+//   // Replace ./data.json with your JSON feed
+//  fetch('https://www.nectune.com/api.json')
+//     .then(response => {
+//       return response.json()
+//     })
+//     .then(data => {
+//       // Work with JSON data here
+//       test = data 
+//     })
+//     .catch(err => {
+//       // Do something for an error here
+//     })
+    
+    async function logFetch(url) {
+      try {
+        const response = await fetch(url);
+        return response.json();
+      }
+      catch (err) {
+        console.log('fetch failed', err);
+      }
     }
     
+    var caca = await logFetch('https://www.nectune.com/api.json');
 
-    //BLESSED LAYOUT
-    // Create a screen object.
-    var screen = blessed.screen({
-      smartCSR: true
-    });
+    console.log (caca)
 
-    screen.title = 'Nectune homepage';
+    // //BLESSED LAYOUT
+    // // Create a screen object.
+    // var screen = blessed.screen({
+    //   smartCSR: true
+    // });
 
-    var mainHome = blessed.box({
-      top: '0%',
-      left: 'center',
-      width: '60%',
-      height: '100%',
-      scrollable: true,
-      keys: true,
-      alwaysScroll: true,
-      scrollbar: {
-        bg: 'blue'
-      },
-    });
+    // screen.title = 'Nectune homepage';
 
-    screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-      return process.exit(0);
-    });
+    // var mainHome = blessed.box({
+    //   top: '0%',
+    //   left: 'center',
+    //   width: '60%',
+    //   height: '100%',
+    //   scrollable: true,
+    //   keys: true,
+    //   alwaysScroll: true,
+    //   scrollbar: {
+    //     bg: 'blue'
+    //   },
+    // });
+
+    // screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+    //   return process.exit(0);
+    // });
     
-    screen.append(mainHome);
+    // screen.append(mainHome);
     
 
-    //START AIRTABLE LOOP
-    base('live').select({
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
 
-      fetchNextPage();
-
-        //MAP RECORD
-        var ids = records.map(function(i) {
-          return{
-            content: i.get('content'),
-            boxWidth: i.get('boxWidth'),
-            boxHeight: i.get('boxHeight'),
-            row: i.get('row'),
-            left: i.get('left'),
-            valign: i.get('valign')
-          } 
-        });
+    //     //MAP RECORD
+    //     var ids = records.map(function(i) {
+    //       return{
+    //         content: i.get('content'),
+    //         boxWidth: i.get('boxWidth'),
+    //         boxHeight: i.get('boxHeight'),
+    //         row: i.get('row'),
+    //         left: i.get('left'),
+    //         valign: i.get('valign'),
+    //         padding: i.get('padding'),
+    //       } 
+    //     });
       
 
-        //PAGINATE SORT RECORD
-        var listCards = paginate(ids, 100, 1)
+    //     //PAGINATE SORT RECORD
+    //     var listCards = paginate(ids, 100, 1)
         
 
-        //LOOP RECORD
-        var i = 0;
-        var toTop = 0;
+    //     //LOOP RECORD
+    //     var i = 0;
+    //     var toTop = 0;
 
-        while (i < listCards.length) {
+    //     while (i < listCards.length) {
 
-        if (i == 1){
-          var toTop = toTop + listCards[0].boxHeight + 4;
-        } 
-        else if (listCards[i].row == 0){
-          var toTop = toTop;
-        } 
-        else{
-          var toTop = toTop + listCards[i].boxHeight + 4;
-        }
+    //     if (i == 1){
+    //       var toTop = toTop + listCards[0].boxHeight + 4;
+    //     } 
+    //     else if (listCards[i].row == 0){
+    //       var toTop = toTop;
+    //     } 
+    //     else{
+    //       var toTop = toTop + listCards[i].boxHeight + 4;
+    //     }
 
-          console.log(toTop)
 
-          //CARD
-          var boxTwo = blessed.box({
-            parent: mainHome,
-            top: toTop + '%',
-            left: listCards[i].left + '%',
-            width: listCards[i].boxWidth + '%',
-            height: listCards[i].boxHeight + '%',
-            content: listCards[i].content,
-            valign: listCards[i].valign,
-            padding: 3,
-            tags: true,
-            border:{
-              type: 'bg',
-              ch:'z',
-              bg: '#4A70B6',
-              fg:'#D386B9',
-            },
-            style: {
-              fg: '#000',
-              bg: '#efefef',
-              bold: true,
-            }
-          });
+    //       //CARD
+    //       var boxTwo = blessed.box({
+    //         parent: mainHome,
+    //         top: toTop + '%',
+    //         left: listCards[i].left + '%',
+    //         width: listCards[i].boxWidth + '%',
+    //         height: listCards[i].boxHeight + '%',
+    //         content: listCards[i].content,
+    //         valign: listCards[i].valign,
+    //         padding: listCards[i].padding,
+    //         tags: true,
+    //         border:{
+    //           type: 'bg',
+    //           ch:'z',
+    //           bg: '#4A70B6',
+    //           fg:'#D386B9',
+    //         },
+    //         style: {
+    //           fg: '#000',
+    //           bg: '#efefef',
+    //           bold: true,
+    //         }
+    //       });
         
-        //LOOP RECORD END
-        i++;
+    //     //LOOP RECORD END
+    //     i++;
           
-        }
+    //     }
 
-      // Render the screen.
-      screen.render();
+    //   // Render the screen.
+    //   screen.render();
               
-      
-    //END AIRTABLE LOOP
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-    });
 
 
   }
